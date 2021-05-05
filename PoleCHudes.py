73e6414a6862fcc7f0e0cf8d821c1e0bd5bf9a9f
Maks_Denisov <72945727+Max1Denysov@users.noreply.github.com>
2021-05-05 23:40:07 +0300

    Update PoleCHudes.py

--- PoleCHudes.py
+++ PoleCHudes.py
@@ -1,58 +1,73 @@
 import random
 import re
-
-l=[]
-c =[]
-cc = []
-ccc= []
-cccc =[]
-c = input("Введите первое секретное слово = ").lower()
-l.append(c)
-cc =input("Введите второе секретное слово = ").lower()
-l.append(cc)
-ccc= input("Введите третье секретное слово = ").lower()
-l.append(ccc)
-cccc= input("Введите четвертое секретное слово = ").lower()
-l.append(cccc)
-print("П О Л Е - Ч У Д Е С")
-ch=input('Введите "старт" чтобы начать игру: ')
-z=random.choice(l)
-x=("-"*len(z))
-noimp,imp=[],[]
-print()
-count,d = 0,0
-while(count<8):
+def my_function():
+    buff = []
+    l=["питон","процессор","ада","алгоритм","лямбда","фрактал"]
+    z=random.choice(l)
+    health = len(z) + 4 # + 4 потому что слишком сложно , при одной ошибке уже не победить.
+    x=("-"*len(z))
+    noimp,imp=[],[]
     print()
-    print(x)
-    n=input("Введите букву:  ").lower()
+    count,d = 0,0
+    i = 0
+ 
     
-    if(n.islower() and (len(n)==1)):
-        if(n in z):
-            if(n in noimp):
-                print("Вы уже ввели эту букву")       
-            else:
-                for m in re.finditer(n, z):
-                    x = x[:m.start()] + n + x[m.start()+1:]
-                noimp.append(n)
-        else:              
-            if(n in imp):
-                print("Вы уже ввели эту букву")             
-            else:
-                print("Такая буква отсутствует")
-                count+=1
-                imp.append(n)
-        if(x == z):
-            print(z)
-            d=1
+    while(count<8):
+        print()
+        print(x)
+        print('Использованные буквы: ',buff)
+        print('колличество попыток : ', health - i)
+        n=input("Введите букву:  ").lower()
+        i+=1
+        buff.append(n)
+        if i== health:
+            print('Вы проиграли')
             break
+        if(n.islower() and (len(n)==1)):
+            if(n in z):
+                if(n in noimp):
+                    print("Вы уже ввели эту букву")       
+                else:
+                    for m in re.finditer(n, z):
+                        x = x[:m.start()] + n + x[m.start()+1:]
+                    noimp.append(n)
+            else:              
+                if(n in imp):
+                    print("Вы уже ввели эту букву")             
+                else:
+                    print("Такая буква отсутствует")
+                    count+=1
+                    imp.append(n)
+            if(x == z):
+                print(z)
+                d=1
+                break
+                
+        else:
+            if(len(n)!=1):
+                print("Вы можете ввести только одну букву")
+            elif(not n.islower()):
+                print("Нет")
+            
+    if(d==1):
+        print("***********************Победа***********************")
+        designUs = input('Не хотите ли повторить?(да\нет) :').lower()
+        if designUs =='да':
+            my_function()
+        if designUs =='нет':
+            print('До свиданья!')
             
     else:
-        if(len(n)!=1):
-            print("Вы можете ввести только одну букву")
-        elif(not n.islower()):
-            print("Нет")
+        print("Попробуйте снова!")
         
-if(d==1):
-    print("""Победа! ---- вы выиграли автомобиль""")
-else:
-    print("Вы проиграли!")
+ 
+
+print("П О Л Е - Ч У Д Е С")
+
+print("Доброго времени суток. Сегодня мы сынраем в игру.Вам нужно назвать букву русского алфавита чтобы угадать слово.Количество попыток ограничено.За один раз можно ввести только одну букву.")
+start=input('Для начала игры введите "старт":').lower()
+if start=="старт":
+    my_function()
+if start != "старт":
+    print('неверно')
+ 
